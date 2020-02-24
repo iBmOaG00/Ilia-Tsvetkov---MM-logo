2bb2aeb43889b891c800485cfe235a571a1758ed
Ilia Tsvetkov <Nikalaskeij2@abv.bg>
2020-02-24 23:58:55 +0200

    initial commit

---
+++ Ilia/Display.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ilia
+{
+    class Display
+    {
+        /// <summary>
+        /// Handles user input and perform verifications.
+        /// </summary>
+        /// <param name="userInput">user's input</param>
+        /// <returns>Number of stars that will be the foundation of the letters. </returns>
+        public int UserInput(string userInput)
+        {
+            bool pass = false;
+            int n;
+
+            //  Let the user define N
+            Console.WriteLine("Write the number of stars 👎 that you want to showing the " +
+                "tickness of the letter M.");
+
+            //  Verify input
+            do
+            {
+                userInput = Console.ReadLine();
+                Int32.TryParse(userInput, out n);
+
+                //  Check if INT
+                if (!Int32.TryParse(userInput, out n))
+                {
+                    Console.WriteLine("Please, enter a whole number.");
+                }
+                //  If the number is higher than 2 and lower than 10000
+                else if (!(n > 2 && n < 10000))
+                {
+                    Console.WriteLine("Could you try with a number lower than 10000 and higher than 2.");
+                }
+                //  Check if the number is ODD
+                else if (n % 2 != 1)
+                {
+                    Console.WriteLine("Please input an ODD number.");
+                }
+                else
+                {
+                    pass = true;
+                }
+            } while (!pass);    // until all the requirement are fulfilled make the user response
+
+
+            return n;
+        }
+
+        /// <summary>
+        /// Display a star.
+        /// </summary>
+        /// <param name="n">The number of "*" you want listed on 1 line.</param>
+        public void StarDisplay(int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("*");
+            }
+        }
+
+        /// <summary>
+        /// Display a dash.
+        /// </summary>
+        /// <param name="n">The number of "-" you want listed on 1 line.</param>
+        public void DashDisplay(int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("–");
+            }
+        }
+
+
+    }
+}
---
+++ Ilia/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ilia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput = "";
+            int n;
+            int dashGrowth;
+            int starGrowth;
+            int dashD = 1;
+            int underH = 1;
+
+            //  In order to call my methods.
+            Display myDisplay = new Display();
+
+            //  Handle user input.
+            n = myDisplay.UserInput(userInput);
+
+            //  Use the number to calculate the growth of the dashes and stars.
+            dashGrowth = n;
+            starGrowth = n;
+
+            int y = n + 1; //   height of the picture (rows)
+
+            // The point when we will start constructing the next part of the letter (UPPER + LOWER part)
+            int changePoint = y / 2;
+
+            //  int x = n * 10; //
+
+            for (int i = 0; i < y; i++)
+            {
+                if (i >= changePoint)
+                {
+                    //  LOWER SIDE
+
+                    //  Letter 1.
+                    myDisplay.DashDisplay(dashGrowth);
+                    myDisplay.StarDisplay(n);
+                    myDisplay.DashDisplay(dashD);
+                    if (dashD == n)
+                    {
+                        //  For the last row of the image
+                        myDisplay.StarDisplay(n);
+                    }
+                    else
+                    {
+                        //  Until we reach the last row of the image calculate the number of stars we need to use.
+                        myDisplay.StarDisplay(n * 2 - underH);
+                    }
+                    myDisplay.DashDisplay(dashD);
+                    myDisplay.StarDisplay(n);
+
+                    // Divider between our 2 letters.
+                    myDisplay.DashDisplay(n - dashD);
+
+                    //  Letter 2
+                    myDisplay.StarDisplay(n);
+                    myDisplay.DashDisplay(dashD);
+                    if (dashD == n)
+                    {
+                        myDisplay.StarDisplay(n);
+                    }
+                    else
+                    {
+                        myDisplay.
+                            StarDisplay((n * 2 - underH));
+                    }
+                    myDisplay.DashDisplay(dashD);
+                    myDisplay.StarDisplay(n);
+                    myDisplay.DashDisplay(dashGrowth);
+                    Console.WriteLine();
+
+                    //  Calculate
+                    underH += 2;
+                    starGrowth += 2;
+                    dashD = dashD + 2;
+                    if (dashGrowth != 0) dashGrowth -= 1;
+
+                }
+                else
+                {
+                    //  UPPER SIDE
+
+                    //  Letter 1
+                    myDisplay.DashDisplay(dashGrowth);
+                    myDisplay.StarDisplay(starGrowth);
+                    myDisplay.DashDisplay(dashGrowth - i);
+                    myDisplay.StarDisplay(starGrowth);
+
+                    // Divider between our 2 letters.
+                    myDisplay.DashDisplay(dashGrowth);
+                    myDisplay.DashDisplay(dashGrowth);
+
+                    // Letter 2.
+                    myDisplay.StarDisplay(starGrowth);
+                    myDisplay.DashDisplay(dashGrowth - i);
+                    myDisplay.StarDisplay(starGrowth);
+                    myDisplay.DashDisplay(dashGrowth);
+                    Console.WriteLine();
+
+                    //  Calculate
+                    starGrowth += 2;
+                    dashGrowth -= 1;
+                }
+            }
+
+
+
+
+
+            //  Pause program
+            Console.ReadLine();
+
+        }
+    }
+}
